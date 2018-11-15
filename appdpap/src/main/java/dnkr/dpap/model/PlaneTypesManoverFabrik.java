8681dd4459b528359e5f4f707bdbba392511b10b
djn <danieljnowak@gmail.com>
2018-11-15 15:16:42 +0100

    wegplan felder ziel per manoever mirrored

--- appdpap/src/main/java/dnkr/dpap/model/PlaneTypesManoverFabrik.java
+++ appdpap/src/main/java/dnkr/dpap/model/PlaneTypesManoverFabrik.java
@@ -4,30 +4,42 @@ import static dnkr.dpap.model.PlaneTypes.VALKYR;
 
 class PlaneTypesManoverFabrik {
 private final PlaneTypes types;
+private PlaneType type;
 
 public PlaneTypesManoverFabrik(PlaneTypes types) {
   this.types = types;
-  add(VALKYR, "vw", 6);
-  addMirrored(VALKYR, "r1", new PlaneManoever("vvvrlvv"));
-  addMirrored(VALKYR, "r2", new PlaneManoever("vvvrvv"));
-  addMirrored(VALKYR, "r3", new PlaneManoever("vvrvrv"));
-  
-  
-  types.getById(KYRASSIER).getPlaneManoevers().put("vw", new PlaneManoever(4));
+  setType(VALKYR);
+  add("vw", 6);
+  addMirrored("r1", new PlaneManoever("vvvrlvv"));
+  addMirrored("r2", new PlaneManoever("vvvrvv"));
+  addMirrored("r3", new PlaneManoever("vvrvrv"));
+  setType(KYRASSIER);
+  add("vw", 4);
+  addMirrored("r1", new PlaneManoever("vvrlv"));
+  addMirrored("r2", new PlaneManoever("vvrv"));
+  addMirrored("r3", new PlaneManoever("vrvrv"));
   //"vvvvv,vvlrvv,vvvlvv,vvlvv,vvrlvv,vvvrvv,vvrvv");
 }
 
-private void add(String typeId, String art, int speed) {
-  types.getById(typeId).getPlaneManoevers().put(art, new PlaneManoever(speed));
+private void add(String art, int speed) {
+  type.getPlaneManoevers().put(art, new PlaneManoever(speed));
 }
 
-private void addMirrored(String typeId, String art, PlaneManoever manoever) {
-  types.getById(typeId).getPlaneManoevers().put(art, manoever);
-  types.getById(typeId).getPlaneManoevers().put(art.replace("r", "l"), manoever.getMirrored());
+private void addMirrored(String art, PlaneManoever manoever) {
+  type.getPlaneManoevers().put(art, manoever);
+  type.getPlaneManoevers().put(art.replace("r", "l"), manoever.getMirrored());
 }
 
-private void add(String typeId, String art, PlaneManoever manoever) {
-  types.getById(typeId).getPlaneManoevers().put(art, manoever);
+private void add(String art, PlaneManoever manoever) {
+  type.getPlaneManoevers().put(art, manoever);
+}
+
+public PlaneType getType() {
+  return type;
+}
+
+public void setType(String typeId) {
+  this.type = types.getById(typeId);
 }
 }
 
